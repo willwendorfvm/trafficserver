32ec396c6933c92c73bc06cb26b15045e0787ab5
Will Wendorf <william.wendorf@verizonmedia.com>
2020-06-10 17:48:22 -0400

    Print

--- tests/gold_tests/remap/remap_reload_twice.test.py
+++ tests/gold_tests/remap/remap_reload_twice.test.py
@@ -81,6 +81,7 @@ lst.append(tr.Processes.Process(
         returncode = 0
     )
 )
+print(tr.Processes)
 tr.Processes.touch-0.StartBefore(Test.Processes.ts, ready=When.FileExists(os.path.join(tr.RunDirectory, 'ts/log/diags.log')))
 tr.Processes.reload-1.StartBefore(tr.Processes.touch-0)
 tr.Processes.touch-2.StartBefore(tr.Processes.reload-1)
