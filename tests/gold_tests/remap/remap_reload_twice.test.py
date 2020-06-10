c86a754be3e267f695440147e59192601a0867db
Will Wendorf <william.wendorf@verizonmedia.com>
2020-06-10 13:40:23 -0400

    Extend delay

--- tests/gold_tests/remap/remap_reload_twice.test.py
+++ tests/gold_tests/remap/remap_reload_twice.test.py
@@ -52,7 +52,7 @@ Test.PreparePlugin(os.path.join(Test.Variables.AtsTestToolsDir, 'plugins', 'rema
 
 # First reload
 tr = Test.AddTestRun("Original reload config")
-tr.DelayStart = 1  # delay start of test run to allow previous command to take effect
+tr.DelayStart = 4  # delay start of test run to allow previous command to take effect
 tr.Processes.Default.StartBefore(Test.Processes.ts, ready=When.PortReady(ts.Variables.port))
 tr.Processes.Default.Command = 'traffic_ctl config reload'
 # Need to copy over the environment so traffic_ctl knows where to find the unix domain socket
@@ -64,6 +64,7 @@ tr.StillRunningAfter = ts
 
 # Second reload
 tr = Test.AddTestRun("Duplicate reload config")
+tr.DelayStart = 4  # delay start of test run to allow previous command to take effect
 tr.Processes.Default.Command = 'traffic_ctl config reload'
 # Need to copy over the environment so traffic_ctl knows where to find the unix domain socket
 tr.Processes.Default.Env = ts.Env
